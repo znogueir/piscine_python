90ca5c4e6eb3e81d509b3fad987b8e4240e526fb
znogueir <zachary.nogueira@gmail.com>
2024-12-09 18:09:25 +0100

    filter (ex06) fixed + docstring

--- 0-starting/ex06/filterstring.py
+++ 0-starting/ex06/filterstring.py
@@ -1,5 +1,9 @@
 def filterstring(cond, it):
-    """TODO"""
-    for x in it:
-        if not cond and x or cond and cond(x):
-            yield x
+    """filter(function or None, iterable) --> filter object
+
+    \rReturn an iterator yielding those items of iterable for which function(item)
+    \ris true. If function is None, return the items that are true.
+    """
+    # Carriage returns in docstring in order to remove the auto indents
+    # from my formatter.
+    return (x for x in it if (not cond and x) or (cond and cond(x)))
