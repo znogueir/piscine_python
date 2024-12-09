316899605365926efd2ec18344c9f3029d39b1af
znogueir <zachary.nogueira@gmail.com>
2024-12-09 20:04:45 +0100

    started to block out main lines of logic

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,3 +1,30 @@
+import os
+
+
 def ft_tqdm(lst: range):
+    """
+    ft_tqdm(lst: range)
+
+    Takes a range as a parameter,
+    Displays a loading bar according to the range, with the speed
+    and estimated end time.
+    """
+    # get the length of the terminal
+    columns, _ = os.get_terminal_size(0)
+    nbr_of_elems = lst[-1] + 1
+    # get the length of the loading bar :
+    # total length - eta string - 5
+    # if the length of loading bar is <= 0:
+    # set it to 1
+    percentage = ""  # "000%"
+    loading_bar = ""  # "|<progress = chars followed by spaces>|"
+    status_infos = ""  # " <i + 1/nbr of elems [time elapsed<estimated duration, <1 or 2 spaces><speed>it/s or s/it]"
+
     for i in lst:
+        # compose the percentage string
+        percentage = int(round((i + 1) / nbr_of_elems * 100))
+        # compose the eta string
+        # pass
+        # output the complete loading bar
+        print(f"{percentage}%|{i}", end="\r")
         yield i
--- 0-starting/ex08/tester.py
+++ 0-starting/ex08/tester.py
@@ -3,10 +3,10 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
-for elem in ft_tqdm(range(333)):
+for elem in ft_tqdm(range(32)):
     sleep(0.1)
 print()
 
-for elem in tqdm(range(333)):
+for elem in tqdm(range(32000000000)):
     sleep(0.1)
 print()
