1ca3ce2cfb6b2e8df00ce2ae4c46641d3272388f
znogueir <zachary.nogueira@gmail.com>
2024-12-10 17:54:44 +0100

    refacto

--- 0-starting/ex08/Loading.py
+++ 0-starting/ex08/Loading.py
@@ -1,54 +1,78 @@
 import os
+import time
+
+
+def render_percentage(progress: float):
+    """
+    Calcualtes the current progress as percentage.
+    Returns a string.
+    """
+    percentage = int(progress * 100)
+    percentage_str = str(percentage)
+    percentage_str = " " * (3 - len(percentage_str)) + percentage_str
+
+    return percentage_str
+
+
+def render_loading_bar(size: int, progress: float):
+    """
+    Calculates the current progress bar.
+    Returns a string.
+    """
+    if size <= 0:
+        size = 1
+
+    progress_size = int(size * progress)
+    loading_bar = "=" * progress_size + " " * (size - progress_size)
+
+    return loading_bar
+
+
+def render_time_estimations(i: int, n: int, start_time: float, curr_time: float):
+    """
+    Calculates the elapsed time, the estimated duration,
+    and the speed of progress.
+    Returns a string."""
+    elapsed = 0
+    duration = None
+    speed_str = "?it/s"  # or s/it if ratio < 1.0
+    elapsed_str = "00:00"
+
+    status = f" {i + 1}/{n} [{elapsed_str}<{duration}, {speed_str}]"
+
+    return status
 
 
 def ft_tqdm(lst: range):
     """
-    ft_tqdm(lst: range)
-
     Takes a range as a parameter,
     Displays a loading bar according to the range, with the speed
     and estimated end time.
+    Yields each number in the range.
     """
-    # get the length of the terminal
-    columns, _ = os.get_terminal_size(0)
 
-    # get the number of elements in the range
-    nbr_of_elems = lst[-1] + 1
+    cols, _ = os.get_terminal_size(0)
+    n = lst[-1] + 1
 
-    percentage = 0  # "000%"
-    loading_bar = ""  # "|<progress = chars followed by spaces>|"
-    status_infos = ""
-    elapsed_time = "--:--"
-    estimated_duration = "--:--"
-    speed = "0.00it/s"  # or s/it if ratio < 1.0
+    start_time = time.time()
+    curr_time = None
+    elapsed = 0
 
     for i in lst:
-        # compose the percentage string
-        percentage = int(round((i + 1) / nbr_of_elems * 100))
-        percentage_str = str(percentage)
-        percentage_str = " " * (3 - len(percentage_str)) + percentage_str
+        progress = (i + 1) / float(n)
 
-        # compose the status string
-        status_infos = (
-            f" {i + 1}/{nbr_of_elems} [{elapsed_time}<{estimated_duration}, {speed}]"
-        )
+        if curr_time is not None:
+            elapsed = curr_time - start_time
+        curr_time = time.time()
 
-        # get the length of the loading bar :
-        loading_bar_length = columns - len(status_infos) - 6
-        if loading_bar_length <= 0:
-            loading_bar_length = 1
-
-        # compute progress bar
-        progress_bar_length = int(percentage / 100.0 * loading_bar_length)
-
-        # assemble loading bar
-        loading_bar = "=" * progress_bar_length + " " * (
-            loading_bar_length - progress_bar_length
-        )
+        # render the 3 main elements of the bar
+        percentage = render_percentage(progress)
+        status = render_time_estimations(i, n, start_time, elapsed)
+        loading_bar = render_loading_bar(cols - len(status) - 6, progress)
 
         # display the complete loading bar
         print(
-            f"{percentage_str}%|{loading_bar}|{status_infos}\r",
+            f"{percentage}%|{loading_bar}|{status}\r",
             end="\r",
         )
 
--- 0-starting/ex08/tester.py
+++ 0-starting/ex08/tester.py
@@ -7,6 +7,6 @@ for elem in ft_tqdm(range(32)):
     sleep(0.1)
 print()
 
-# for elem in tqdm(range(32000000000)):
-#     sleep(0.1)
+# for elem in tqdm(range(3200000)):
+#     sleep(2)
 print()
