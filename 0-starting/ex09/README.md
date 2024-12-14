1139f4b7f974b016b25d588811ff91d682811227
znogueir <zachary.nogueira@gmail.com>
2024-12-14 20:16:16 +0100

    understood packaging, only missing the actual code.

--- 0-starting/ex09/README.md
+++ 0-starting/ex09/README.md
@@ -1,4 +1,8 @@
-## ft-package
+# ft-package
 
 This is a sample test package.
 
+## Usage
+
+`python setup.py sdist bdist_wheel`
+`pip install .`
---
+++ 0-starting/ex09/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    description="A sample test package",
+    url="https://github.com/eagle/ft_package",
+    author="eagle",
+    author_email="eagle@42.fr",
+    license="MIT",
+    packages=find_packages(),
+)
