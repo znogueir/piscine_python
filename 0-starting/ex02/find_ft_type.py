767755f8699830e5271a305dac227af8fb503448
znogueir <zachary.nogueira@gmail.com>
2024-12-16 17:03:57 +0100

    updated logic

--- 0-starting/ex02/find_ft_type.py
+++ 0-starting/ex02/find_ft_type.py
@@ -1,10 +1,10 @@
-p = {list: "List :", tuple: "Tuple :", set: "Set :", dict: "Dict :"}
+from typing import Any
 
 
-def all_thing_is_obj(object: any) -> int:
+def all_thing_is_obj(object: Any) -> int:
     t = type(object)
-    if t in p:
-        print(p[t], t)
+    if t in [list, tuple, set, dict]:
+        print(str(t).split("'")[1].capitalize(), ":", t)
     elif isinstance(object, str):
         print(object, "is in the kitchen :", str)
     else:
